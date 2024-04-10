e727950ae8987fd6462555b6075c4e5a5d728d14
Bianca Kioko <kiokobianca01@gmail.com>
2024-04-10 12:19:21 +0300

    added fetch request

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  fetch('https://anapioficeandfire.com/api/books')
+    return fetch('https://anapioficeandfire.com/api/books')
+     .then(response => response.json())
+     .then(json => renderBooks(json))
   
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
